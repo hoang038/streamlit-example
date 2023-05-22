ab571f1d217bc574530b052135932c245b243684
hoang038 <hoang038@gmail.com>
2023-05-22 16:09:27 +0700

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ with st.form('my_form'):
 if submitted:
      st.markdown('')
 else:
-    st.write('☝️ Place your order!')'''
+    st.write('☝️ Place your order!')
 
 import streamlit as st
 import numpy as np
@@ -172,4 +172,24 @@ def load_data_b():
 
 st.write(load_data_b())
 b1 = time()
-st.info(b1-b0)
+st.info(b1-b0)'''
+
+
+import streamlit as st
+
+st.title('st.session_state')
+
+def lbs_to_kg():
+  st.session_state.kg = st.session_state.lbs/2.2046
+def kg_to_lbs():
+  st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header('Input')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("Pounds:", key = "lbs", on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
+
+st.header('Output')
+st.write("st.session_state object:", st.session_state)
