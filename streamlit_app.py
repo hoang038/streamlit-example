1e1abd53e334395294464d49ae92543bff6e0497
hoang038 <hoang038@gmail.com>
2023-05-22 16:26:41 +0700

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,7 +172,7 @@ def load_data_b():
 
 st.write(load_data_b())
 b1 = time()
-st.info(b1-b0)'''
+st.info(b1-b0)
 
 
 import streamlit as st
@@ -192,4 +192,36 @@ with col2:
   kilogram = st.number_input("Kilograms:", key = "kg", on_change = kg_to_lbs)
 
 st.header('Output')
-st.write("st.session_state object:", st.session_state)
+st.write("st.session_state object:", st.session_state)'''
+
+# How to use API by building the Bored API app
+import streamlit as st
+import requests
+
+st.title('🏀 Bored API app')
+
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
