a14801ce070b94987342d2cd9ef150679c32ea75
hoang038 <hoang038@gmail.com>
2023-05-22 15:23:15 +0700

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ if cola:
 # Using st.secrets
 import streamlit as st
 st.title('st.secrets')
-st.write(st.secrets['message']) '''
+st.write(st.secrets['message']) 
 
 # Upload file scv lên.
 import streamlit as st
@@ -94,4 +94,21 @@ if uploaded_file is not None:
   st.subheader('Descriptive Statistics')
   st.write(df.describe())
 else:
-  st.info('☝️ Upload a CSV file')
+  st.info('☝️ Upload a CSV file') '''
+
+import streamlit as st
+import time
+
+st.title('st.progress')
+
+with st.expander('About this app'):
+     st.write('You can now display the progress of your calculations in a Streamlit app with the `st.progress` command.')
+
+my_bar = st.progress(0)
+
+for percent_complete in range(100):
+     time.sleep(0.05)
+     my_bar.progress(percent_complete + 1)
+
+st.balloons()
+
